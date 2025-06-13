e812290c96abf531dc1a02d45c2fffc9fce19449
MohammedRaghib <mohammedmraghib@gmail.com>
2025-06-13 11:20:49 +0300

    imported ii8n in app js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import AppNavigator from "./navigation/AppNavigator";
 import { CheckInfoProvider } from "./services/UserContext";
+import './Language/i18n.jsx';
 
 export default function App() {
   return (
