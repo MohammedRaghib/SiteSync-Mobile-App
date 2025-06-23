be8f48431b20d9661636aeb0a5b27e900e6422d4
MohammedRaghib <mohammedmraghib@gmail.com>
2025-06-23 12:39:00 +0300

    Additions For Push Notifications
    added notification handler, useeffect in app js for notification checking
    changed app name in app.json
    new google service json for new app
    installed expo-constants
    added project id to getpushtokenasync

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import AppNavigator from "./navigation/AppNavigator";
 import { CheckInfoProvider } from "./services/UserContext";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+import { Platform } from "react-native";
 import "./Language/i18n.jsx";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
 export default function App() {
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    const subscription = Notifications.addNotificationReceivedListener((notification) => {
+      console.log("📩 Foreground Notification:", notification);
+    });
+
+    return () => subscription.remove();
+  }, []);
+
   return (
     <CheckInfoProvider>
-
       <AppNavigator />
-      
     </CheckInfoProvider>
   );
 }
